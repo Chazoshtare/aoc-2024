6b4ff679ff3ec752ce46a6cc3fcef00f906b4e57
Chazoshtare <chazoshtare@protonmail.com>
2024-12-13 03:08:43 +0100

    Day 8 part 1 solution

--- src/day8.rs
+++ src/day8.rs
@@ -1,43 +1,74 @@
+use std::collections::hash_map::Entry;
+use std::collections::{HashMap, HashSet};
+
 pub fn solve_part1(input: &str) -> u32 {
-    0
+    let max_index = input.lines().next().unwrap().len() - 1;
+    let antennas: HashMap<char, Vec<Location>> = locate_antennas(input);
+    let anti_nodes: HashSet<Location> = antennas
+        .iter()
+        .flat_map(|(frequency, locations)| get_anti_nodes(locations))
+        .filter(|location| location.is_valid(max_index as i32))
+        .collect();
+    anti_nodes.len() as u32
 }
 
 pub fn solve_part2(input: &str) -> u32 {
     0
 }
 
-fn locate_antennas(input: &str) -> Vec<Antenna> {
-    input
-        .lines()
-        .enumerate()
-        .flat_map(|(y, line)| {
-            line.chars().enumerate().filter_map(move |(x, char)| {
-                if char != '.' {
-                    Some(Antenna {
-                        frequency: char,
-                        location: Location {
-                            x: x as i32,
-                            y: y as i32,
-                        },
-                    })
-                } else {
-                    None
+fn locate_antennas(input: &str) -> HashMap<char, Vec<Location>> {
+    let mut antennas: HashMap<char, Vec<Location>> = HashMap::new();
+    input.lines().enumerate().for_each(|(y, line)| {
+        line.chars().enumerate().for_each(|(x, char)| {
+            if char != '.' {
+                let location = Location {
+                    x: x as i32,
+                    y: y as i32,
+                };
+                match antennas.entry(char) {
+                    Entry::Vacant(mut entry) => {
+                        entry.insert(vec![location]);
+                    }
+                    Entry::Occupied(mut entry) => {
+                        entry.get_mut().push(location);
+                    }
                 }
-            })
+            }
         })
-        .collect()
+    });
+    antennas
 }
 
-#[derive(Debug)]
-struct Antenna {
-    frequency: char,
-    location: Location,
+fn get_anti_nodes(antennas: &Vec<Location>) -> Vec<Location> {
+    let count = antennas.len();
+    let mut anti_nodes: Vec<Location> = Vec::new();
+    for i in 0..count {
+        for j in 0..count {
+            if i != j {
+                anti_nodes.push(antennas[i].mirror(&antennas[j]));
+            }
+        }
+    }
+    anti_nodes
 }
 
-#[derive(Debug)]
+#[derive(Debug, Hash, Eq, PartialEq)]
 struct Location {
     x: i32,
     y: i32,
+}
+
+impl Location {
+    fn is_valid(&self, max_index: i32) -> bool {
+        self.x >= 0 && self.x <= max_index && self.y >= 0 && self.y <= max_index
+    }
+
+    fn mirror(&self, center: &Location) -> Location {
+        Location {
+            x: self.x * 2 - center.x,
+            y: self.y * 2 - center.y,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -79,6 +110,6 @@ mod tests {
 ............
 ";
         let result = solve_part2(input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 34);
     }
 }
