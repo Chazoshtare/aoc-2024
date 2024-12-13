7b6d0b4e51fcb33a8b9884bc5efdf728349a4a3e
Chazoshtare <chazoshtare@protonmail.com>
2024-12-13 03:36:06 +0100

    Day 8 part 2 solution

--- src/day8.rs
+++ src/day8.rs
@@ -6,14 +6,21 @@ pub fn solve_part1(input: &str) -> u32 {
     let antennas: HashMap<char, Vec<Location>> = locate_antennas(input);
     let anti_nodes: HashSet<Location> = antennas
         .iter()
-        .flat_map(|(frequency, locations)| get_anti_nodes(locations))
+        .flat_map(|(_, locations)| get_anti_nodes(locations))
         .filter(|location| location.is_valid(max_index as i32))
         .collect();
     anti_nodes.len() as u32
 }
 
 pub fn solve_part2(input: &str) -> u32 {
-    0
+    let max_index = input.lines().next().unwrap().len() - 1;
+    let antennas: HashMap<char, Vec<Location>> = locate_antennas(input);
+    let anti_nodes: HashSet<Location> = antennas
+        .iter()
+        .flat_map(|(_, locations)| get_harmonic_anti_nodes(locations, max_index as i32))
+        .filter(|location| location.is_valid(max_index as i32))
+        .collect();
+    anti_nodes.len() as u32
 }
 
 fn locate_antennas(input: &str) -> HashMap<char, Vec<Location>> {
@@ -26,7 +33,7 @@ fn locate_antennas(input: &str) -> HashMap<char, Vec<Location>> {
                     y: y as i32,
                 };
                 match antennas.entry(char) {
-                    Entry::Vacant(mut entry) => {
+                    Entry::Vacant(entry) => {
                         entry.insert(vec![location]);
                     }
                     Entry::Occupied(mut entry) => {
@@ -52,7 +59,21 @@ fn get_anti_nodes(antennas: &Vec<Location>) -> Vec<Location> {
     anti_nodes
 }
 
-#[derive(Debug, Hash, Eq, PartialEq)]
+fn get_harmonic_anti_nodes(antennas: &Vec<Location>, max_index: i32) -> Vec<Location> {
+    let count = antennas.len();
+    let mut anti_nodes: Vec<Location> = Vec::new();
+    for i in 0..count {
+        for j in 0..count {
+            if i != j {
+                let mut all_harmonics = antennas[i].all_steps(&antennas[j], max_index);
+                anti_nodes.append(&mut all_harmonics);
+            }
+        }
+    }
+    anti_nodes
+}
+
+#[derive(Hash, Eq, PartialEq)]
 struct Location {
     x: i32,
     y: i32,
@@ -63,11 +84,31 @@ impl Location {
         self.x >= 0 && self.x <= max_index && self.y >= 0 && self.y <= max_index
     }
 
+    fn add(&self, x: i32, y: i32) -> Location {
+        Location {
+            x: self.x + x,
+            y: self.y + y,
+        }
+    }
+
     fn mirror(&self, center: &Location) -> Location {
         Location {
             x: self.x * 2 - center.x,
             y: self.y * 2 - center.y,
         }
+    }
+
+    fn all_steps(&self, other: &Location, max_index: i32) -> Vec<Location> {
+        let difference_x = self.x - other.x;
+        let difference_y = self.y - other.y;
+        let mut locations: Vec<Location> = Vec::new();
+        let mut location = other.add(difference_x, difference_y);
+        while location.is_valid(max_index) {
+            let next_location = location.add(difference_x, difference_y);
+            locations.push(location);
+            location = next_location;
+        }
+        locations
     }
 }
 
@@ -111,5 +152,21 @@ mod tests {
 ";
         let result = solve_part2(input);
         assert_eq!(result, 34);
+    }
+
+    #[test]
+    fn solves_part2_t_example() {
+        let input = "T.........
+...T......
+.T........
+..........
+..........
+..........
+..........
+..........
+..........
+..........";
+        let result = solve_part2(input);
+        assert_eq!(result, 9);
     }
 }
